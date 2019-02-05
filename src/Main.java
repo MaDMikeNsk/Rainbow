6b46c0d71c95a906e91d01200ed2de46269fa368
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-02-05 11:36:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    private static void flipArray(String[] array) {
+        if (array.length!=0) {
+            for (int i = 0; i < array.length / 2; i++) {
+                String temp = array[i];
+                array[i] = array[array.length - 1 - i];
+                array[array.length - 1 - i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+	    String[] rainbow = {"Red","Orange","Yellow","Green","Cyan","Blue","Violet"};
+
+	    for (int i = rainbow.length-1; i>=0; i--){
+            System.out.println(rainbow[i]);
+        }
+        System.out.println("================");
+	    flipArray(rainbow);
+        for (int i = 0; i<rainbow.length; i++){
+            System.out.println(rainbow[i]);
+        }
+    }
+}
